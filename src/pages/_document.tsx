a9dd8badc7c62003459c7f36f72bf12e43c63a19
Sungjung Jo <toysmoon@gmail.com>
2020-12-03 16:37:56 +0900

    fix: fonts update

---
+++ src/pages/_document.tsx
@@ -0,0 +1,22 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@400;500;700&display=swap"
+            rel="stylesheet"
+          />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
