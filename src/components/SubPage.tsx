280b1c789dfca290fde380d5b87fdb01d245b324
toysmoon <toysmoon@gmail.com>
2021-12-21 18:09:01 +0900

    fix: 추천 페이지 max-width 적용

--- src/components/SubPage.tsx
+++ src/components/SubPage.tsx
@@ -14,47 +14,49 @@ const SubPage: FC<SubPageProps> = ({ title, children }) => {
   return (
     <Page>
       <div>
-        <span onClick={() => router.back()}>X</span>
-        <h1>{title}</h1>
-        <style jsx>{`
-          div {
-            position: fixed;
-            top: 0;
-            left: 0;
-            right: 0;
-            height: 50px;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            background: ${isDarkMode ? 'black' : 'white'};
-            box-shadow: 0 2px 0 0 ${isDarkMode ? 'white' : 'black'};
-          }
-          span {
-            position: absolute;
-            left: 20px;
-            top: 50%;
-            font-size: 1.5rem;
-            transform: translateY(-50%);
-            cursor: pointer;
-          }
-          h1 {
-            font-size: 1.5rem;
-            font-weight: normal;
-          }
-        `}</style>
-      </div>
-      <div>
-        {children}
-        <style jsx>{`
-          div {
-            position: fixed;
-            top: 52px;
-            left: 0;
-            right: 0;
-            bottom: 0;
-            overflow: scroll;
-          }
-        `}</style>
+        <div>
+          <span onClick={() => router.back()}>X</span>
+          <h1>{title}</h1>
+          <style jsx>{`
+            div {
+              position: fixed;
+              top: 0;
+              height: 50px;
+              width: 100%;
+              max-width: 500px;
+              display: flex;
+              justify-content: center;
+              align-items: center;
+              background: ${isDarkMode ? 'black' : 'white'};
+              box-shadow: 0 2px 0 0 ${isDarkMode ? 'white' : 'black'};
+            }
+            span {
+              position: absolute;
+              left: 20px;
+              top: 50%;
+              font-size: 1.5rem;
+              transform: translateY(-50%);
+              cursor: pointer;
+            }
+            h1 {
+              font-size: 1.5rem;
+              font-weight: normal;
+            }
+          `}</style>
+        </div>
+        <div>
+          {children}
+          <style jsx>{`
+            div {
+              position: fixed;
+              top: 52px;
+              bottom: 0;
+              width: 100%;
+              max-width: 500px;
+              overflow: scroll;
+            }
+          `}</style>
+        </div>
       </div>
     </Page>
   );
